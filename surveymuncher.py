9192c5f07d3d07245edde404b5a863d6f3cb255e
digzilla <jurgen@digzilla.org>
2016-10-21 22:31:42 +0100

    - Fixed error in testing suite #4 - Added error log entry for empty csvfile variable - Made assumption that forward slashes will be used in the csvfile variable - this will need to be made more robust in future

--- surveymuncher.py
+++ surveymuncher.py
@@ -6,20 +6,24 @@
 
 
 # ReadCsv reads a .csv file, validates it and outputs the data as a list of tuples, and also a line count and errors
+# todo: work out how to best feed in paths - just expects a file in the same folder for now or with forward slashes
 class ReadCsv:
     def __init__(self, csvfile):
         # start errorlog with empty list
         self.errorlog = []
         # check if there is a / for splitting
-        if csvfile.find('\\') != -1:
+        if csvfile.find('/') != -1:
             # split filename by searching for / and taking what's after it
-            self.filename = csvfile.rsplit('\\',1)[1]
+            self.filename = csvfile.rsplit('/',1)[1]
             # split filepath by splitting with filename
-            self.filepath = csvfile.split(self.filename, 1)[0]
+            self.filepath = csvfile.split(self.filename,1)[0]
         else:
+            if csvfile = '':
+                self.errorlog.append('The input \'csvfile\' is empty')
+            else:
+                self.errorlog.append('The input \'csvfile\' does not seem to contain a path (no \'/\' found)')
             self.filename = csvfile
             self.filepath = ''
-            self.errorlog.append('The input with the name does not seem to contain a path and a filename')
         # start linecounter at 0
         self.linecounter = 0
         # start readtext as an empty list
@@ -33,36 +37,36 @@ print('class ReadCsv - test suite')
 print('**************************')
 print('\n')
 
-# file with relative path and two backslashes
-testfile1 = ReadCsv('\\test_data\\AFOR-survey-combined-v01-edited.csv')
-print('testfile1 - file with relative path and two backslashes')
+# file with relative path and two slashes
+testfile1 = ReadCsv('/test_data/AFOR-survey-combined-v01-edited.csv')
+print('testfile1 - file with relative path and two slashes')
 print('filepath = '+testfile1.filepath)
 print('filename = '+testfile1.filename)
 print('errorlog = '+str(testfile1.errorlog))
 print('-------------------')
 
-# file with no path and no backslashes
+# file with no path and no slashes
 testfile2 = ReadCsv('AFOR-survey-combined-v01-edited.csv')
-print('testfile2 - file with no path and no backslashes')
+print('testfile2 - file with no path and no slashes')
 print('filepath = '+testfile2.filepath)
 print('filename = '+testfile2.filename)
 print('errorlog = '+str(testfile2.errorlog))
 print('-------------------')
 
-# file with no path and one backslash
-testfile3 = ReadCsv('\\AFOR-survey-combined-v01-edited.csv')
-print('testfile3 - file with no path and one backslash')
+# file with no path and one slash
+testfile3 = ReadCsv('/AFOR-survey-combined-v01-edited.csv')
+print('testfile3 - file with no path and one slash')
 print('filepath = '+testfile3.filepath)
 print('filename = '+testfile3.filename)
 print('errorlog = '+str(testfile3.errorlog))
 print('-------------------')
 
-# file with full path and multiple backslashes
-file4
-testfile4 = ReadCsv('S:\\Allway\\The Vault\\Archive - Stage 1 (roughly gathered)\\Projects\\2016\\20161015 - GPS Auditor\\surveymuncher\\test_data\\AFOR-survey-combined-v01-edited.csv')
-print('testfile3 - file with no path and one backslash')
-print('filepath = '+testfile3.filepath)
-print('filename = '+testfile3.filename)
+# file with full path and multiple slashes
+testfile4 = ReadCsv('S:/Allway/The Vault//Archive - Stage 1 (roughly gathered)/Projects/2016/20161015 - GPS Auditor' +
+                    '/surveymuncher/test_data/AFOR-survey-combined-v01-edited.csv')
+print('testfile4 - file with full path and multiple slashes')
+print('filepath = '+testfile4.filepath)
+print('filename = '+testfile4.filename)
 print('errorlog = '+str(testfile3.errorlog))
 print('-------------------')
 
